bc14922e875be4fd6208432903a7da67709563fd
Sveta Kryukova <116656921+Sveta-Kryukova@users.noreply.github.com>
2026-04-01 15:24:15 +0300

    feature/footer (#2)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { AppFooterComponent } from "./layout/app-footer/app-footer.component";
 import { AppHeaderComponent } from "./layout/app-header/app-header.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, AppHeaderComponent],
+  imports: [RouterOutlet, AppHeaderComponent, AppFooterComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
